839e88eeff0779be84fc0c255996992f03c57577
Sudeep Veggalam <47568981+sudeep-v@users.noreply.github.com>
2020-06-01 19:07:52 +0530

    Add files via upload

---
+++ Manual1/codes/ee18btech11045/ee18btech11045.py
@@ -0,0 +1,29 @@
+import numpy as np 
+from numpy import poly1d
+import control as ct 
+import matplotlib.pyplot as plt 
+
+Gden = poly1d([1,10,100])
+
+Gden = Gden.c
+G = ct.tf([100],Gden)
+
+t_end = 2
+t = np.linspace(0,t_end,500)
+
+x,y = ct.step_response(G,t)
+
+overshoot = round(max(y),2)
+final_value = round(y[len(t)-1],2)
+
+plt.hlines(overshoot, xmin = 0, xmax = t_end, color = 'red')
+plt.text(0,overshoot+0.02, overshoot, ha='left', va='center')
+
+plt.hlines(final_value, xmin = 0, xmax = t_end, color = 'red')
+plt.text(0,final_value+0.02, final_value, ha='left', va='center')
+
+plt.plot(x,y)
+plt.xlabel("time")
+plt.ylabel("c(t)")
+plt.grid()
+plt.show()
